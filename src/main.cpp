6cc8047ae58a6921fceeebf406ea8568d033d542
EmirDns <76104355+EmirDns@users.noreply.github.com>
2022-06-21 16:59:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 #define MOTOR_NUM 1
 
 //Class Declarations
-AccelStepper myStepper(1,2,4);  //2 -> STEP , 3 -> DIR
+AccelStepper myStepper(1,2,3);  //2 -> STEP , 3 -> DIR
  
 //Function Declarations
-void multiArrToFloatArr(std_msgs::Float64MultiArray &given_multi_arr, float*dest_float_arr);
+void multiArrToFloatArr(std_msgs::Float64MultiArray given_multi_arr, float*dest_float_arr);
 void scaraCommandCallBack(const std_msgs::Float64MultiArray &scara_command_array);
 
 //Variables
@@ -22,32 +22,31 @@ std_msgs::Float64MultiArray deneme_array;
 //ROS Classes
 ros::NodeHandle nh;
 ros::Publisher scara_feedback_publisher("scara_feedback_topic", &scara_feedback_array);
-ros::Subscriber<std_msgs::Float64MultiArray> scara_command_subscriber("deneme_topic", scaraCommandCallBack);
+ros::Subscriber<std_msgs::Float64MultiArray> scara_command_subscriber("multiarray_topic", scaraCommandCallBack);
 
-ros::Publisher deneme_pub("deneme_topic", &deneme_array);
+ros::Publisher deneme_publisher("deneme_topic", &deneme_array);
 
 void setup()
 {   
     nh.initNode();
     nh.advertise(scara_feedback_publisher);
     nh.subscribe(scara_command_subscriber);
-    nh.advertise(deneme_pub);
-    
+
+    nh.advertise(deneme_publisher);
+        
     scara_feedback_array.data=(float*)malloc(sizeof(float)*MOTOR_NUM);
     scara_feedback_array.data_length=MOTOR_NUM;
 
     deneme_array.data=(float*)malloc(sizeof(float)*MOTOR_NUM);
     deneme_array.data_length=MOTOR_NUM;
-    for(int i=0;i<MOTOR_NUM;i++){
-        deneme_array.data[i]=-1600;
-    }
 
     myStepper.setMaxSpeed(10000);
 }
 
 
 void loop()
-{       
+{   
+    //myStepper.moveTo(3200);    
     myStepper.moveTo(scara_command_float_arr[0]);
     myStepper.setSpeed(1000);
     myStepper.runSpeedToPosition();
@@ -57,7 +56,8 @@ void loop()
         nh.spinOnce();
         scara_feedback_array.data[0]=myStepper.currentPosition();
         scara_feedback_publisher.publish(&scara_feedback_array);
-        deneme_pub.publish(&deneme_array);
+
+        deneme_publisher.publish(&deneme_array);
     }
 }
 
@@ -69,5 +69,6 @@ void multiArrToFloatArr(std_msgs::Float64MultiArray given_multi_arr, float*dest_
 }
 
 void scaraCommandCallBack(const std_msgs::Float64MultiArray &scara_command_array){  
-    multiArrToFloatArr(scara_command_array, scara_command_float_arr);
+    deneme_array.data[0]=scara_command_array.data[1]*1600;
+    multiArrToFloatArr(deneme_array, scara_command_float_arr);
 }
